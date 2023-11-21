609acdc651c634e4f6d0825e7190b6d47e59fdef
Hassaan Maajid <hassaan.majid@crowdbotics.com>
2023-11-21 15:53:32 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "artiste-44551",
-  url: "https://artiste-44551.botics.co",
-  api: "https://artiste-44551.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "artiste-44551",
+  "url": "https://artiste-44551.botics.co",
+  "api": "https://artiste-44551.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "SignUp"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
